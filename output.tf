1aed51a520022137207e61bf1dcf359daa45c5ed
ganesh95dos <62937061+ganesh95dos@users.noreply.github.com>
2025-07-09 18:19:10 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,20 +1,24 @@
 output "public_ip" {
-    value = aws_instance.my_instance.public_ip  
+    value = aws_instance.my_tws_instance.public_ip  
 }
 
 output "public_dns" {
-    value = aws_instance.my_instance.public_dns  
+    value = aws_instance.my_tws_instance.public_dns  
 }
 
 output "instance_id" {
-    value = aws_instance.my_instance.id  
+    value = aws_instance.my_tws_instance.id  
 }
 
 output "instance_state" {
-    value = aws_instance.my_instance.instance_state  
+    value = aws_instance.my_tws_instance.instance_state  
 }
 
 
 output "instance_ami" {
-    value = aws_instance.my_instance.ami 
+    value = aws_instance.my_tws_instance.ami 
+}
+
+output "aws_region" {
+  value = data.aws_region.current.name
 }
